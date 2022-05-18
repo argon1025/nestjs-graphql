e4be78da1d3d04859994576667e279554cc12751
Seongrok.lee <55491354+argon1025@users.noreply.github.com>
2022-05-18 18:28:57 +0900

    GraphQL 테스트 구성 (#6)
    * feat: PrismaService 추가
    
    * chore: graphql apollo 패키지 추가
    
    * chore: configModule 추가, 환경설정 파일명 변경
    
    * feat: GraphQL테스트 파일 작성

---
+++ lib/prisma/prisma.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+
+import { PrismaService } from 'lib/prisma/prisma.service';
+
+@Module({
+  providers: [PrismaService],
+  exports: [PrismaService],
+})
+export class PrismaModule {}
