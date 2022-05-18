9b2756347e71fed5a15869350f72c213f3d5d058
argon1025 <argon1025@gmail.com>
2022-05-18 17:12:33 +0900

    feat: PrismaService 추가

---
+++ lib/prisma/prisma.service.ts
@@ -0,0 +1,31 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    super({
+      log: [
+        { emit: 'stdout', level: 'query' },
+        { emit: 'stdout', level: 'info' },
+        { emit: 'stdout', level: 'warn' },
+        { emit: 'stdout', level: 'error' },
+      ],
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  // NOTE:  NestJS 종료 이벤트가 실행되기 전에 Prisma는 process.exit()를 호출하기때문에 해당 이벤트 수신 구문을 추가한다
+  // https://github.com/prisma/prisma/issues/2917#issuecomment-708340112
+  async onModuleDestroy() {
+    this.$on('beforeExit', async () => {
+      await this.$disconnect();
+    });
+  }
+}
